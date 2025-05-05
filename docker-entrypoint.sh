db19712662b6cd8b2f86bc96507d96acb326d2fa
Denis Aydo <pythonist001@proton.me>
2025-05-05 18:06:07 +0300

    Add scripts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
-# Запускаем myst в фоне
 /usr/bin/myst --mmn.api-key="$MYST_API_KEY" --vendor.id=AYDO service --agreed-terms-and-conditions &
 MYST_PID=$!
 
-# Функция для завершения myst при остановке контейнера
 cleanup() {
   echo "Stopping myst (PID $MYST_PID)..."
   kill $MYST_PID
